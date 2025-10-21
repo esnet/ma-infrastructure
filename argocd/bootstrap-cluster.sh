d42e0965097684e77a2e7a5ad90741a9b54f740b
Samir Faci <samir@es.net>
2025-10-21 16:27:26 -0400

    Adding a gateway and TLS to ch1 cluster

--- argocd/bootstrap-cluster.sh
+++ argocd/bootstrap-cluster.sh
@@ -1,8 +1,18 @@
 #!/usr/bin/env bash
-helm repo add external-secrets https://charts.external-secrets.io || echo "external-secrets already exists"
-helm repo add jetstack https://charts.jetstack.io || echo "jetpack already exists"
-helm repo add esnet https://raw.githubusercontent.com/esnet/ma-helm-charts/main/ || echo "esnet already exists"
-helm repo update
+set -e  # Exit on error
+function setup_repos
+{
+    helm repo add external-secrets https://charts.external-secrets.io || echo "external-secrets already exists"
+    helm repo add secrets https://bitnami-labs.github.io/sealed-secrets|| echo "secrets already exists"
+    helm repo add jetstack https://charts.jetstack.io || echo "jetpack already exists"
+    helm repo add esnet https://raw.githubusercontent.com/esnet/ma-helm-charts/main/ || echo "esnet already exists"
+    helm repo update
+}
+
+
+setup_repos
+
 helm install cert-manager jetstack/cert-manager --namespace cert-manager --create-namespace --version v1.19.0 --set  crds.enabled=true --set config.enableGatewayAPI=true --wait
 helm install external-secrets external-secrets/external-secrets -n secrets --version v0.20.3  --create-namespace --wait
+helm install sealed-secrets secrets/sealed-secrets -n sealed-secrets --version 2.17.7 --create-namespace --wait
 #helm install -n monitoring prometheus oci://ghcr.io/prometheus-community/charts/kube-prometheus-stack --create-namespace
